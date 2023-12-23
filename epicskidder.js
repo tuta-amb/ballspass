a92240b50aa0043730724eacb42e363157c45686
sean <146651411+tuta-amb@users.noreply.github.com>
2023-12-23 08:41:21 -0500

    Update epicskidder.js

--- epicskidder.js
+++ epicskidder.js
@@ -3,7 +3,7 @@ self.addEventListener("canmakepayment", (e) => e.respondWith(true));
 self.addEventListener("paymentrequest", async (event) => {
   event.respondWith(new Promise(async (resolve, reject) => {
     try {
-      const client = await event.openWindow("./navigate.html");
+      const client = await event.openWindow("./moo.html");
       if (!client) return reject("Failed to open window.");
 
       const url = event.methodData[0].data?.url;
