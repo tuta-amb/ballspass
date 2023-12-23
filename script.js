047dce71f15297524e1f154b506dd0358129225f
sean <146651411+tuta-amb@users.noreply.github.com>
2023-12-23 08:35:50 -0500

    adsf

---
+++ script.js
@@ -0,0 +1,22 @@
+function buypass() {
+  if (!window.PaymentRequest) return alert("Payment Request API is not available! :wompwomp:");
+
+  new PaymentRequest(
+    [{
+      supportedMethods: location.origin + "/ballsment.json",
+      data: {
+        url: document.querySelector("input").value
+      },
+    }],
+    {
+      total: {
+        label: "no homo pass",
+        amount: {
+          value: "69420", currency: "USD"
+        },
+      },
+    }
+  ).show();
+}
+
+document.querySelector("button").onclick = buypass;
